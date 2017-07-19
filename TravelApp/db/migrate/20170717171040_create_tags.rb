6ae83b7bd80ce730404f9611ee5ed34c756cb2a9
Saber Hosney <saber20_13@hotmail.com>
2017-07-19 21:02:12 +0200

    use heroku branch for deploy

--- TravelApp/db/migrate/20170717171040_create_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateTags < ActiveRecord::Migration
-  def change
-    create_table :tags do |t|
-      t.string :title
-      t.string :image
-      t.timestamps null: false
-    end
-  end
-end
