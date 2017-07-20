4409fc043c138cbafdec46ea1ac0da37fb9ed467
pro-devel-linux <saber20_13@hotmail.com>
2017-07-20 20:58:12 +0200

    styling pages and add some dynamic links

---
+++ podcast/app/controllers/podcasts_controller.rb
@@ -0,0 +1,5 @@
+class PodcastsController < ApplicationController
+
+	
+
+end
---
+++ podcast/app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all.order("created_at DESC")
+	end
+
+	def show
+		@users = User.find(params[:id])
+	end
+end
