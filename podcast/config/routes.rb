83c568075952f0ebd5966de27d2697406cf5c16f
pro-devel-linux <saber20_13@hotmail.com>
2017-07-22 23:29:24 +0200

    added dashboard for signed user

--- podcast/config/routes.rb
+++ podcast/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-root 'welcome#index'
 
-resources :users, only: [:index, :show] do
-	resources :episodes
-end
-  
+	devise_for :users
+
+	resources :users, only: [:index, :show] do
+		resources :episodes
+	end
+
+	authenticated :user do 
+		root 'users#dashboard', as: "authenticated_root"
+	end
+
+	root 'welcome#index'
 end
