17eb7b1d75b285bb9177d04b88d19a8d39ce9006
pro-devel-linux <saber20_13@hotmail.com>
2017-07-22 20:40:28 +0200

    Add episode model and add associations

---
+++ podcast/db/migrate/20170722183408_create_episodes.rb
@@ -0,0 +1,11 @@
+class CreateEpisodes < ActiveRecord::Migration
+  def change
+    create_table :episodes do |t|
+      t.string :title
+      t.text :description
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
